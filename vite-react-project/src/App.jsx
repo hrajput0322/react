62f3d3b4451746fc54dee7260a8d5efad3adfedb
hrajput0322 <hrajput0322@gmail.com>
2023-11-29 10:26:33 +0530

    commited

---
+++ vite-react-project/src/App.jsx
@@ -0,0 +1,12 @@
+import Content from "./Content"
+
+function App() {
+  return (
+    <>
+      <Content/>
+      <p>testing to render multiple html tags</p>
+    </>
+  )
+}
+
+export default App
---
+++ vite-react-project/src/Content.jsx
@@ -0,0 +1,10 @@
+function Content() {
+    return (
+        <>
+            <h1>Hello Harsh</h1>
+            <h3>Bye Harsh</h3>
+        </>
+    )
+}
+
+export default Content
